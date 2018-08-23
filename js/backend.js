e272c16b8f724ec9d022818e4d7c9ab45ee93fb1
sidali250 <sidali250@gmail.com>
2018-08-23 17:08:26 +0000

    fin du jquery button scroll-to-top

--- js/backend.js
+++ js/backend.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
 		$('.nav-list').slideToggle()
 	});
 
+	//rendre navbar backgroundColor black apres scroll 100px
 	$(window).scroll(function(){
 		var sc = $(this).scrollTop();
 		if(sc > 100){
@@ -15,6 +16,7 @@ $(document).ready(function(){
 		}
 	});
 
+	//relier a de navbar avec scrollTop des sections
 	$('nav .nav-list .list a').click(function(e){
 		e.preventDefault();
 		$('html, body').animate({
@@ -25,5 +27,32 @@ $(document).ready(function(){
 	//ajout de la class active au liste li du navbar
 	$('nav .nav-list .list a').click(function(){
 		$(this).addClass('active').parent().siblings().find('a').removeClass('active');
+	});
+
+	
+	$(window).scroll(function(){
+		//suchronize scroll with classActive of link of navbar
+		$('.sections2').each(function(){
+			if(($(window).scrollTop() + 90) > $(this).offset().top){
+				var sectionsId = $(this).attr('id');
+				$('nav .nav-list .list a').removeClass('active');
+				$('nav .nav-list .list a[data-scroll="' + sectionsId + '"]').addClass('active');
+			}
+		});
+
+		//sychronize scroll with button scroll-to-top
+		if($(window).scrollTop() >= 1000){
+			$('.scroll-to-top').fadeIn(400);
+		}else{
+			$('.scroll-to-top').fadeOut(400);
+		}
+	});
+
+	// Event click of button scroll-to-top
+	$('.scroll-to-top').click(function(event){
+		event.preventDefault();
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
 	});
 });
